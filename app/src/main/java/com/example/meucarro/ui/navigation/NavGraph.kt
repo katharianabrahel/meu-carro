3f299b6eae0579769686f01c599ea6a5ef90fb0e
Hitalo Nascimento <hitalobruno2011@gmail.com>
2025-04-09 21:25:27 -0300

    verifica se tem token salvo e direciona para home

--- app/src/main/java/com/example/meucarro/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/meucarro/ui/navigation/NavGraph.kt
@@ -1,29 +1,36 @@
 package com.example.meucarro.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.meucarro.ui.screens.HomePage
+import com.example.meucarro.ui.screens.LaunchScreen
 import com.example.meucarro.ui.screens.LoginScreen
 import com.example.meucarro.ui.screens.SignUpScreen
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "login") {
-            composable("home") {
-            HomePage(navController)
-            }
+    NavHost(navController = navController, startDestination = "launch") {
+        composable("launch") {
+            LaunchScreen(navController)
+        }
 
-            composable("login") {
-            LoginScreen(onSignUpClick = {
-                navController.navigate("signup")
-            },
+        composable("home") {
+            HomePage(navController)
+        }
+
+        composable("login") {
+            LoginScreen(
+                onSignUpClick = {
+                    navController.navigate("signup")
+                },
                 onLoginSuccess = {
-                navController.navigate("home") {
-                    popUpTo("login") { inclusive = true }
-                }
-            })
+                    navController.navigate("home") {
+                        popUpTo("login") { inclusive = true }
+                    }
+                })
         }
         composable("signup") {
             SignUpScreen(onSignUpSuccess = {
---
+++ app/src/main/java/com/example/meucarro/ui/screens/LaunchScreen.kt
@@ -0,0 +1,48 @@
+package com.example.meucarro.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import com.example.meucarro.services.database.user_preferences.UserPreferences
+
+@Composable
+fun LaunchScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val userPrefs = remember { UserPreferences(context) }
+    val isChecking = remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        val token = userPrefs.getToken()
+        isChecking.value = false
+        if (!token.isNullOrBlank()) {
+            navController.navigate("home") {
+                popUpTo("launch") { inclusive = true }
+            }
+        } else {
+            navController.navigate("login") {
+                popUpTo("launch") { inclusive = true }
+            }
+        }
+    }
+
+    if (isChecking.value) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
